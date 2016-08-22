a022fac260966c80a37199a6951205caa157e645
U-hp\admin <admin@hp.(none)>
2016-08-23 00:52:06 +0800

    ServerEndAndReactBootstrap

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+console.log("start...");
+var cjs=fs.readFile('package.json', 'utf8', function (err, txt) {
+  console.log("step 1...");
+  if (err) {
+    throw err;
+  }
+  console.log("step 2...");
+  var djs=fs.readFile('app.js', 'utf8', function (err, content) {
+    console.log("step 3...");
+    if (err) {
+      throw err;
+    }
+    console.log("step 4...");
+    console.log(content);
+  });
+  console.log("step 5...");
+});
+
+console.log("over...");
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
